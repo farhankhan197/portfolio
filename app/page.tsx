e9c52dd526be19896a3fe4a8a60dd77dbf6e8dc0
Farhan Khan <farhankhan.code@gmail.com>
2025-03-10 12:00:21 +0000

    update photo

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { ModeToggle } from "@/components/ModeToggle";
 import Link from "next/link";
 import { useState, JSX } from "react";
-import { ChevronRight, LinkIcon,Instagram, Menu } from "lucide-react";
+import { ChevronRight, LinkIcon, Instagram, Menu } from "lucide-react";
 import dynamic from "next/dynamic";
 import { motion } from "framer-motion";
 import LinkedinIcon from "@/components/LinkedinIcon";
@@ -39,10 +39,9 @@ export default function Home(): JSX.Element {
       icon: <GithubIcon className="h-5 w-5" />,
     },
     {
-      name:"Twitter",
-      url:"https://twitter.com/Farhankhan_twt",
-      icon : <TwitterIcon className="h-5 w-5" />,
-
+      name: "Twitter",
+      url: "https://twitter.com/Farhankhan_twt",
+      icon: <TwitterIcon className="h-5 w-5" />,
     },
     {
       name: "LeetCode",
@@ -63,7 +62,7 @@ export default function Home(): JSX.Element {
       name: "Discord",
       url: "/Farhan_Khan_Resume.pdf",
       icon: <DiscordIcon className="h-5 w-5" />,
-    }
+    },
   ];
 
   const toggleMenu = (): void => setIsMenuOpen(!isMenuOpen);
@@ -78,13 +77,13 @@ export default function Home(): JSX.Element {
                 dark:border-black/20 rounded-lg shadow-lg"
       >
         <div className="flex items-center gap-4">
-          <Image
+          {/* <Image
             src="/images/profile.jpg"
             alt="Profile"
             width={40}
             height={40}
             className="rounded-full"
-          />
+          /> */}
           <h1 className="text-md font-semibold shining-text">Starfield</h1>
         </div>
         <div className="hidden absolute left-1/2 transform -translate-x-1/2  md:flex space-x-4">
@@ -111,12 +110,19 @@ export default function Home(): JSX.Element {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8 }}
-        className="text-center mb-10"
+        className="text-center mb-4 "
       >
-        <p className="text-gray-600 dark:text-gray-400 m-2 p-4">
-          Hey there, i'm
-        </p>
-        <h1 className="text-5xl font-extrabold">Farhan Khan</h1>
+        <p className="text-gray-600 dark:text-gray-400  ">Hey there, i'm</p>
+        <div className="flex items-center justify-center">
+        <Image
+          src="/images/photo.jpg"
+          alt="Profile"
+          width={300}
+          height={0}          
+          className="h-auto m-4 flex items-center justify-center rounded-full"
+        />
+        </div>
+        <h1 className="text-3xl font-extrabold">Farhan Khan</h1>
         <p className="text-gray-600 flex flex-start dark:text-gray-400 mt-2 text-lg text-left p-4">
           I am a third year CS student, Passionate Full-Stack Developer and
           Machine Learning Engineer with expertise in building high-performance
@@ -130,28 +136,28 @@ export default function Home(): JSX.Element {
         </p>
       </motion.div>
       <motion.div
-        className="flex gap-4 -mt-4"
+        className="flex gap-auto max-w-1/2 items-center justify-center"
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.6, delay: 0.4 }}
-      > 
+      >
         {socialLinks.map((link) => (
           <Link
             key={link.name}
             href={link.url}
-            className="p-3 bg-gray-300 dark:bg-gray-700 rounded-full hover:bg-gray-400 dark:hover:bg-gray-600 transition"
+            className="p-3  bg-gray-300 dark:bg-gray-700 rounded-full hover:bg-gray-400 dark:hover:bg-gray-600 transition"
           >
             {link.icon}
           </Link>
         ))}
       </motion.div>
-        <button className="inline flex items-center gap-2 bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-400 px-3 py-1 rounded-full text-sm font-medium hover:bg-green-200 dark:hover:bg-green-700 transition-colors whitespace-nowrap">
-          <span className="relative flex h-3 w-3">
-            <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
-            <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
-          </span>
-          Available
-        </button>
+      <button className="inline m-4 flex items-center gap-2 bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-400 px-3 py-1 rounded-full text-sm font-medium hover:bg-green-200 dark:hover:bg-green-700 transition-colors whitespace-nowrap">
+        <span className="relative flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+        </span>
+        Available
+      </button>
 
       {/* Sections */}
       {[
