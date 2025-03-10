e5b00b55ebe0bc892ba47b845a9864c3aee9fbdf
Farhan Khan <farhankhan.code@gmail.com>
2025-03-10 09:39:37 +0530

    Update StarField.tsx

--- components/StarField.tsx
+++ components/StarField.tsx
@@ -108,7 +108,7 @@ const StarfieldBackground = () => {
 
   function setup() {
     if (stars.length === 0) {
-      stars = Array.from({ length: 10}, () => new Star());
+      stars = Array.from({ length: 5}, () => new Star());
     } else {
       stars = stars.map((existingStar) => new Star(existingStar));
     }
