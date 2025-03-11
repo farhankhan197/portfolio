c35658689b9a0d0143f782122303c7a3df5c4f2e
Farhan Khan <farhankhan.code@gmail.com>
2025-03-11 11:38:34 +0000

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -300,7 +300,7 @@ export default function Home(): JSX.Element {
               {project.techStack && (
                 <div className="flex gap-2">
                   {project.techStack.map((tech) => (
-                    <p key={tech} className="text-xs dark:bg-gray-200 dark:text-black text-white bg-gray-800 rounded-sm">
+                    <p key={tech} className="text-xs dark:bg-gray-200 dark:text-black border text-white bg-gray-800">
                       {tech}
                     </p>
                   ))}
