400c7045a2428c02d81e1d21400208df1fcf7cd5
Farhan Khan <farhankhan.code@gmail.com>
2025-03-12 03:49:39 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -42,10 +42,10 @@ export default function Navbar() {
         className={`fixed left-0 right-0 flex justify-center z-40 transition-all duration-500 ease-in-out shadow-none transform ${
           isScrolled
             ? `top-0 shadow-none scale-100 px-8 py-4 bg-white dark:bg-[#0d0d0d] ${
-                isOpen ? "shadow-none" : "shadow-md dark:shadow-[0_10px_30px_rgba(255,255,255,0.1)]"
+                isOpen ? "shadow-none" : "shadow-md dark:shadow-none]"
               } rounded-none`
-            : `top-0 scale-100 max-w-5xl px-6 py-4 mx-6 bg-white dark:bg-[#0d0d0d] ${
-                isOpen ? "shadow-none" : "shadow-lg dark:shadow-[0_20px_40px_rgba(255,255,255,0.1)]"
+            : `top-0 scale-100 max-w-5xl px-6 py-4 bg-white dark:bg-[#0d0d0d] ${
+                isOpen ? "shadow-none" : "shadow-lg dark:shadow-none"
               } rounded-lg`
         }`}
       >
