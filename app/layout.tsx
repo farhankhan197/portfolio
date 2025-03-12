58d832574f0e44b6eb34d3cc08fcbd3d2103b8ca
Farhan Khan <farhankhan.code@gmail.com>
2025-03-12 14:21:15 +0530

    add icons in metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,20 @@ import StarfieldBackground from "@/components/StarField";
 export const metadata: Metadata = {
   title: "Farhan Khan",
   description: "a portfolio inspired by space.",
+icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-32x32.png",
+    apple: "/apple-touch-icon.png",
+    other: [
+      { rel: "icon", type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+      { rel: "icon", type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+      { rel: "icon", type: "image/png", sizes: "192x192", url: "/android-chrome-192x192.png" },
+      { rel: "icon", type: "image/png", sizes: "512x512", url: "/android-chrome-512x512.png" }
+    ]
+  },
+  manifest: "/site.webmanifest"
 };
+
 
 export default function RootLayout({
   children,
