a696da2aa4e2eabdf2e836ac4d4ad19983aa74fe
Farhan Khan <farhankhan.code@gmail.com>
2025-03-12 14:49:15 +0530

    Add social links

--- app/page.tsx
+++ app/page.tsx
@@ -210,6 +210,30 @@ export default function Home(): JSX.Element {
             high-performance web applications and AI-driven solutions.
           </p>
         </motion.div>
+      <motion.div
+        className="flex gap-4 w-full max-w-xl sm:max-w-sm md:max-w-lg items-center justify-center px-4"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0.4 }}
+      >
+        {socialLinks.map((link) => (
+          <Link
+            key={link.name}
+            href={link.url}
+            className="bg-gray-300 dark:bg-gray-800 rounded-full hover:bg-gray-400 dark:hover:bg-gray-600 transition"
+          >
+            {link.icon}
+          </Link>
+        ))}
+      </motion.div>
+
+      <button className="inline-flex m-6 items-center gap-2 bg-green-100 dark:bg-green-800 text-green-800 dark:text-green-400 px-3 py-1 rounded-full text-sm font-medium hover:bg-green-200 dark:hover:bg-green-700 transition-colors whitespace-nowrap">
+        <span className="relative flex h-3 w-3">
+          <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
+          <span className="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
+        </span>
+        Available
+      </button>
 
         {/* Work Experience */}
         <motion.h1
