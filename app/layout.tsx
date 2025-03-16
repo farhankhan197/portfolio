5c91695a1f5350e87ad32ad268c2887a1228115d
Farhan Khan <farhankhan.code@gmail.com>
2025-03-16 18:16:10 +0530

    Update layout.tsx
    make dark the default theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <body className={GeistMono.className}>
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
